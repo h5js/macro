c41e698f5da383369bd19550e49847a98193ef57
zhanyuan.lizhy <zhanyuan.lizhy@alibaba-inc.com>
2016-08-10 08:26:22 +0800

    remove strict mode.

--- macro.js
+++ macro.js
@@ -497,7 +497,7 @@ if(urls = script.getAttribute('macro')) {
       url = purl(url, home);
       code = make(context, url);
       code = fixing(code);
-      code = '"use strict";\n' + code + '\n//# sourceURL=' + url;
+      code += '\n//# sourceURL=' + url;
       window.eval(code);
     }
 }
@@ -511,7 +511,7 @@ script.text = code;
 
 if(run) {
   if (url = script.getAttribute('name')) {
-    code = '"use strict";\n' + code + '\n//# sourceURL=' + purl(url, home + '/');
+    code += '\n//# sourceURL=' + purl(url, home + '/');
   }
   window.eval(code);
 }
