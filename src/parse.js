2bc87a031ed68d59a133e29eb14eec0bb5f48aaa
zhanyuan.lizhy <zhanyuan.lizhy@alibaba-inc.com>
2016-12-06 17:11:15 +0800

    passed

--- src/parse.js
+++ src/parse.js
@@ -1,24 +1,41 @@
 // parse.js ----------------------------------------------------------------------------------------
 
 // parse = Parse(grammar)
-var Parse = bind(function (toProduce, toSymbol, reTag, reNext, bad, grammar) {
-    var s, p, i, name,
-      symbols = object();
+var Parse = function () {
+  var reSplitGrammar = /(?:^|[ \t]*\n)[ \t]*(?:\n[ \t]*)+|\s*$/;
+  var reSplitBlock = /[ \t]*\n[ \t]*/;
+  var reSymbol = /(?:\/.*?\/|[A-Za-z]\w*|\d+)[?+*]?/g;
+  var tags = '?+*';
+  var reNext = /\s*(?:\S+|$)/g;
 
-    grammar = grammar.split(this);
+  return Parse;
 
-    for (s = grammar.length; s--;) {
-      var block = grammar[s];
+  function Parse(grammar) {
+    if (isString(grammar)) {
+      grammar = init(grammar);
+    }
+    linkGrammar(grammar);
+    return bind(parse, grammar);
+  }
+
+  // 创建语法的基础符号表
+  function init(code) {
+    var grammar = object();
+    code = split(code, reSplitGrammar);
+
+    for (var s = code.length; s--;) {
+      var block = code[s];
       if (block) {
-        i = block.indexOf(':');
-        if (name = block.substr(0, i).trim()) {
+        var i = indexOf(block, ':');
+        var name = trim(slice(block, 0, i));
+        if (name) {
           var symbol = object();
-          block = block.slice(i + 1).trim();
-          block = block.split(toProduce);
-          for (p = 0; p < block.length; p++) {
-            var row = block[p];
+          block = trim(slice(block, i + 1));
+          block = split(block, reSplitBlock);
+          var row;
+          for (var p = 0; row = block[p]; p++) {
             var produce = object();
-            if (row = row.match(toSymbol)) {
+            if (row = match(row, reSymbol)) {
               for (i = 0; i < row.length; i++) {
                 produce[i] = row[i];
               }
@@ -29,189 +46,170 @@ var Parse = bind(function (toProduce, toSymbol, reTag, reNext, bad, grammar) {
             }
             symbol[p] = produce;
           }
-          symbol.l = p;
-          var tag = '?+*'.indexOf(name.slice(-1));
-          if(tag>=0) {
-            name = name.slice(0, -1);
-          }
-          if(name[0] == '#') {
-            name = name.slice(1);
+          p = indexOf(tags, slice(name, -1)) + 2;
+          symbol.m = p % 2;
+          if (p > 1)
+            name = slice(name, -1);
+          if (name[0] == '#') {
+            name = slice(name, 1);
             symbol.t = name;
           }
-          if(!(tag%2)) symbol.o = 1;
-
-          symbol._ = 1;   //未连接状态
-          symbols[name] = symbol;
+          grammar[name] = symbol;
         }
       }
     }
+    grammar[0] = symbol;  //默认根符号
+    return grammar;
+  }
 
-    var root = name;  //根符号名
-
-    var names = Object.getOwnPropertyNames(symbols);
-    for (s = 0; s < names.length; s++) {
-      name = names[s];
-      make(name);
+  function linkGrammar(grammar) {
+    var names = getOwnPropertyNames(grammar);
+    for (var i = names.length; i--;) {
+      link(names[i]);
     }
-
-    function make(name) {
-      var symbol = symbols[name], source, p;
-      if (!symbol) {
-        var tag = '?+*'.indexOf(name.slice(-1));
-
-        if (name[0] == '/') {  //正则式
-          source = name.slice(1, tag < 0 ? -1 : -2);
-          symbols[name] = symbol = RegExp(source + '|', 'g');
-        }
-        else if (tag<0) {
-          throw error('Undefined symbol: %s', name);
-        }
-        else {
-          symbols[name] = symbol = object();  //要先占位symbolName，否则会死循环
-          source = make(name.slice(0, -1));
-          for (p = 0; p < source.l; p++)
-            symbol[p] = source[p];
+    function link(name) {
+      var symbol = grammar[name], p, i;
+      if (symbol) {
+        if (!symbol.l) {
+          symbol.l = -1;
+          var produce, item;
+          for (p = 0; produce = symbol[p]; p++)
+            for (i = 0; item = produce[i]; i++)
+              produce[i] = link(produce[i]);
           symbol.l = p;
-          if(source.t)
+        }
+      }
+      else {
+        i = indexOf(tags, slice(name, -1)) + 2;
+        var source = i > 1 ? slice(name, 0, -1) : name;
+        if (name[0] == '/') {
+          grammar[name] = symbol = RegExp(slice(source, 1, -1) + '|', 'g');
+        }
+        else if (i > 0) {
+          grammar[name] = symbol = object();  //要先占位symbolName，否则会死循环
+          source = link(source);
+          for (p = 0; produce = source[p]; p++)
+            symbol[p] = produce;
+          symbol.l = p;
+          if (source.t)
             symbol.t = source.t;
         }
-        if(!(tag%2)) symbol.o = 1;
-        if(tag>0) symbol.g = 1;
-      }
-      else if (symbol._) {    //未连接
-        delete symbol._;      //要先删除
-        for (p = 0; p < symbol.l; p++) {
-          var produce = symbol[p];
-          for (i = 0; i < produce.l; i++) {
-            produce[i] = make(produce[i]);
-          }
+        else {
+          throw error('Undefined symbol: %s', name);
         }
+        symbol.m = i % 2;
+        symbol.g = +(i > 2);
       }
-
       return symbol;
     }
+  }
 
-    // token|err = readSymbol(symbol, serial)
-    function readSymbol(symbol, serial) {
-      var token;
-      if (symbol.exec) {   //正式表达式
-        if (token = get(symbol, serial)) {
-          serial.i = symbol.lastIndex;
-        }
-        else {
-          token = serial.i;
-        }
-      }
-      else {
-        token = serial.i;
-        for (var p = 0; p < symbol.l; p++) {
-          var produce = symbol[p];
-          var item = readProduce(produce, serial);
-          if(!bad(item)) {
-            if(symbol.t) {
-              item.t = symbol.t;
-              for(var s=0; s<item.l; s++) {
-                var o = item[s];
-                if(o.t) {
-                  var ts = item[o.t] || (item[o.t] = []);
-                  ts.push(o);
-                }
-              }
-            }
-            token = item;
-            break;
-          }
-          else if(token<item){
-            token = item;
-          }
-        }
-      }
-      return token;
-    }
-
-    function readProduce(produce, serial) {
-      var token = object(), len = 0, back = serial.i;
-      for (var i = 0; i < produce.l; i++) {
-        var symbol = produce[i];
-
-        var g = 0;
-        do {
-          var item = readSymbol(symbol, serial);
-
-          if(bad(item)) break;
-
-          if(item.length || symbol.t) {
-            token[len++] = item;
-          }
-          else {
-            for(var s=0; s<item.l; s++) {
-              token[len++] = item[s];
-            }
-          }
-          g ++;
-        } while(symbol.g);
-
-        if (!symbol.o && !g) {
-          serial.i = back;    //回溯
-          return item;
-        }
-      }
-      if (len) {
-        token.l = len;
-      }
-      else {
-        token = serial.i;
-      }
-      return token;
-    }
-
-    function get(re, serial) {
-      var token;
-      re.lastIndex = serial.i;
-      if (token = re.exec(serial.s)) {
-        return token[0];
-      }
-    }
-
-    // parse(symbol, code)
-    return function (code, name) {
-      var symbol = symbols[name || root], serial = {s: code, i: 0}, token, err;
-      if (symbol) {
-        token = readSymbol(symbol, serial);
-        if(!symbol.o) {
-          if(typeof token=='number'){
-            serial.i = token;
-            throw error('Syntax error: %s', get(reNext, serial) || '<EOF>');
-          }
-        }
-        else {
-          if(typeof token=='number') {
-            token = undefined;
-          }
-        }
-
-        if(serial.i<code.length) {
+  // parse(symbol, code)
+  function parse(code, name) {
+    var symbol = this[name || 0], serial = {s: code, i: 0}, token;
+    if (symbol) {
+      token = readSymbol(symbol, serial);
+      if (symbol.m) {
+        if (isInteger(token)) {
+          serial.i = token;
           throw error('Syntax error: %s', get(reNext, serial) || '<EOF>');
         }
       }
-      else {
-        throw error('Unknown symbol %s.', name);
+      else if (isInteger(token)) {
+        token = undefined;
       }
-      return token;
-    };
-  },
-  // this:
-  /(?:^|[ \t]*\n)[ \t]*(?:\n[ \t]*)+|\s*$/,
-  //toProduce:
-  /[ \t]*\n[ \t]*/,
-  // reProduce:
-  /(?:\/.*?\/|[A-Za-z]\w*|\d+)[?+*]?/g,
-  // reTag:
-  /[?+*]$/,
-  // reNext:
-  /\s*(?:\S+|$)/g,
-  //bad():
-  function(token){
-    return typeof token==='number';
+
+      if (serial.i < code.length) {
+        throw error('Syntax error: %s', get(reNext, serial) || '<EOF>');
+      }
+    }
+    else {
+      throw error('Unknown symbol %s.', name);
+    }
+    return token;
   }
-);
+
+
+  // token|err = readSymbol(symbol, serial)
+  function readSymbol(symbol, serial) {
+    var token;
+    if (symbol.exec) {   //正式表达式
+      if (token = get(symbol, serial)) {
+        serial.i = symbol.lastIndex;
+      }
+      else {
+        token = serial.i;
+      }
+    }
+    else {
+      token = serial.i;
+      for (var p = 0; p < symbol.l; p++) {
+        var produce = symbol[p];
+        var item = readProduce(produce, serial);
+        if (!isInteger(item)) {
+          if (symbol.t) {
+            item.t = symbol.t;
+            for (var s = 0; s < item.l; s++) {
+              var o = item[s];
+              if (o.t) {
+                var ts = item[o.t] || (item[o.t] = []);
+                ts.push(o);
+              }
+            }
+          }
+          token = item;
+          break;
+        }
+        else if (token < item) {
+          token = item;
+        }
+      }
+    }
+    return token;
+  }
+
+  function readProduce(produce, serial) {
+    var token = object(), len = 0, back = serial.i;
+    for (var i = 0; i < produce.l; i++) {
+      var symbol = produce[i];
+
+      var g = 0;
+      do {
+        var item = readSymbol(symbol, serial);
+
+        if (isInteger(item)) break;
+
+        if (item.length || symbol.t) {
+          token[len++] = item;
+        }
+        else {
+          for (var s = 0; s < item.l; s++) {
+            token[len++] = item[s];
+          }
+        }
+        g++;
+      } while (symbol.g);
+
+      if (symbol.m && !g) {
+        serial.i = back;    //回溯
+        return item;
+      }
+    }
+    if (len) {
+      token.l = len;
+    }
+    else {
+      token = serial.i;
+    }
+    return token;
+  }
+
+  function get(re, serial) {
+    var token;
+    re.lastIndex = serial.i;
+    if (token = re.exec(serial.s)) {
+      return token[0];
+    }
+  }
+
+}();
